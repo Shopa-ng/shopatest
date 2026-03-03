4608513fc7fd4e0a0fb9da3c8d18b31a61dd565c
shopanigeria <shopanigeria@gmail.com>
2026-03-03 20:53:31 +0100

    Update redis.config.ts

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -4,5 +4,6 @@ export default registerAs('redis', () => ({
   host: process.env.REDIS_HOST || 'localhost',
   port: parseInt(process.env.REDIS_PORT || '6379', 10),
   password: process.env.REDIS_PASSWORD || undefined,
-  ttl: parseInt(process.env.REDIS_TTL || '300', 10), // default 5 minutes
+  ttl: parseInt(process.env.REDIS_TTL || '300', 10),
+  tls: process.env.REDIS_TLS === 'true',
 }));
