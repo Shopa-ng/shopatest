233bdc5f2e79a319fbe8af18d6ad7e13a704bfc1
shopanigeria <shopanigeria@gmail.com>
2026-05-13 18:38:57 +0100

    recent

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { NotificationsModule } from '../modules/communication/notifications';
 import { OrderExpiryTask } from './order-expiry.task';
 
 @Module({
+  imports: [NotificationsModule],
   providers: [OrderExpiryTask],
 })
 export class TasksModule {}
